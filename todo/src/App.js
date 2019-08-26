e1fb03924a6e792310128f0db9a1839158b81643
Molly Graham <mollyclaire.graham@gmail.com>
2019-08-26 14:19:53 -0500

    Added logic for click functionality.

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import './App.css';
+import todosData from "./todosData";
 
-function App() {
-  return (
-    <div className="app">
-      <h2>hi</h2>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      todos: todosData
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(id) {
+    this.setState(prevState => {
+      const updatedTodos = prevState.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo
+      })
+      return {
+        todos: updatedTodos
+      }
+    })
+  }
+  render() {return (
+      <div className="app">
+        
+      </div>
+    );
+  }
+  
 }
 
 export default App;
