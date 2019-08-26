4a641a7cac03596286492c31b551b6030cd2c47f
Molly Graham <mollyclaire.graham@gmail.com>
2019-08-26 14:42:28 -0500

    Fixed styling of cards and added comments.

--- todo/src/App.js
+++ todo/src/App.js
@@ -11,27 +11,36 @@ class App extends React.Component {
     }
     this.handleChange = this.handleChange.bind(this);
   }
-
+  // this method is passed to everyone one of our todo items (see under render method)
   handleChange(id) {
     this.setState(prevState => {
+      // the map looks through the todos array to find an id that matches with the parameter id
       const updatedTodos = prevState.todos.map(todo => {
         if (todo.id === id) {
+          // if the todo is completed, it should now be equal to the opposite of what it currently is
           todo.completed = !todo.completed
         }
         return todo
       })
+      // a brand new array (with the updated checked items) replaces the "old" array
       return {
         todos: updatedTodos
       }
     })
   }
   render() {
-    const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange}/>)
+    const todoItems = this.state.todos.map(item => 
+    <TodoItem 
+      key={item.id} 
+      item={item} 
+      handleChange={this.handleChange}/>)
     
     return (
+      <div className="body">
         <div className="todo-list">
             {todoItems}
         </div>
+      </div>
     )    
   }
   
--- todo/src/TodoItem.js
+++ todo/src/TodoItem.js
@@ -5,7 +5,10 @@ function TodoItem(props) {
         <div className="todo-item">
             <input 
                 type="checkbox" 
+                // checked determines whether the input box should be checked or not
                 checked={props.item.completed} 
+                // onChange takes an empty function and passes a method with the parameter of props.item.id. 
+                // In this way, the id of what is clicked is passed along to the handleChange method.
                 onChange={() => props.handleChange(props.item.id)}
             />
             <p>{props.item.text}</p>
